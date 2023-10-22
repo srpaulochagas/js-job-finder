aa9e50baecb3e623e0dbd45bd2251bcc70b4dfe3
Paulo Chagas <paulofilhochagas@gmail.com>
2023-10-21 22:54:12 -0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const app = express();
+const db = require("./db/connection");
+const Job = require("./models/Job");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
+const PORT = 3000;
+
+app.listen(PORT, function () {
+  console.log(`Rodando na porta ${PORT}`);
+});
+
+app.use(express.urlencoded({ extended: false }));
+
+//handle bars
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//db connection
+db.authenticate()
+  .then(() => {
+    console.log("Conectou com sucesso ao banco");
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro ao conectar", err);
+  });
+
+//routes
+app.get("/", (req, res) => {
+  let search = req.query.job;
+  let query = "%" + search + "%";
+
+  if (!search) {
+    Job.findAll({ order: [["createdAt", "DESC"]] })
+      .then((jobs) => {
+        res.render("index", {
+          jobs,
+        });
+      })
+      .catch((err) => console.log(err));
+  } else {
+    Job.findAll({
+      where: { title: { [Op.like]: query } },
+      order: [["createdAt", "DESC"]],
+    })
+      .then((jobs) => {
+        res.render("index", {
+          jobs, search
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+});
+
+//jobs routes
+app.use("/jobs", require("./routes/jobs"));
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Job Finder
+
+Este é um projeto simples de "Job Finder" em JavaScript que permite aos usuários procurar e listar vagas de emprego. O projeto foi desenvolvido como parte de um exercício para praticar JavaScript.
+
+## Funcionalidades
+
+- Pesquisa de vagas de emprego por palavra-chave.
+- Exibição de resultados com informações essenciais, como título, empresa.
+- Possibilidade de clicar em uma vaga para ver detalhes adicionais.
+
+## Tecnologias Utilizadas
+- JavaScript
+- Handlebars
+- CSS
+- Bootstrap
+
+## Como Usar
+
+1. Clone este repositório:
+
+   ```bash
+   git clone https://github.com/srpaulochagas/js-job-finder.git
